a386affd51beadc3d0757d14739378cb902e2ef0
tarek797 <tarek.soliman797@gmail.com>
2023-01-19 20:43:49 +0100

    fetch data to the client from the server

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const server = 'http://127.0.0.1:3000'
+  const [posts, setposts] = useState([])
 
+  useEffect(() => {
+    axios.get(server)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  },[0])
   return (
     <div className="App">
-     
+     <h1>posts</h1>
+     <ul>
+      {
+        posts.map(post => <li key={post.id}>{post.API}</li>)
+      }
+     </ul>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const host = '127.0.0.1'
 const port = 3000
 const app = express()
  
+app.use(cors())
 app.use(express.json())
 
 app.get('/', async function (req, res) {
