a59ae769b4fabd1ae28102bb8c26f5a4e7fa4869
Tarek Habash <tarek.soliman797@gmail.com>
2023-01-19 15:39:17 +0100

    read data from mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const request = require('request')
-const MongoClient = require('mongodb').MongoClient
+import express from 'express'
+import request from 'request'
+import cors from 'cors'
+import {MongoClient} from 'mongodb'
+
 const uri  = 'mongodb+srv://Siciliamia:Siciliamia@cluster0.p50nxkz.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(uri);
+const port = 3000
+const app = express()
+ 
+app.use(cors)
+app.use(express.json())
+
+app.get('/', function (req, res) {
+    res.send("Hello from the server");
+})
+
+app.use("*", (req, res) => res.status(404).json({error: "not found"}))
 
 function saveDataToMongodb() {
     async function createAPIsCollection(client, APIsArray) {
@@ -28,9 +39,23 @@ async function findAPIByName(client, APIName) {
     await client.connect()
     const result = await client.db('APIsdb').collection('APIsCollection')
     .findOne({API: APIName})
+
     console.log(result)
 }
 
+async function findAPIdata(client) {
+    await client.connect()
+    const result = await client.db('APIsdb').collection('APIsCollection')
+    .find({}).sort({length: -1}).limit(20).toArray()
+    console.log(result)
+}
+
+app.get('/', (req, res) => {
+    res.send('welcome')
+})
+
 findAPIByName(client, 'Axolotl')
 
-app.listen(port, () => {console.log(`server is running on port ${5000}`)})
+findAPIdata(client)
+
+app.listen(port, () => {console.log(`Server running at https://${hostname}:${port}/`)})
